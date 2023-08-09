0cfddc222403c4655c3d9ec6649acf2ff03908e1
Pedro Haka <pedrohaka94@gmail.com>
2023-08-09 18:31:55 -0300

    add initial commentary

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const App = () => {
-    return <div>Blue Youtube</div>
+    return <div>Blutube</div>
 }
 
 export default App
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { ReactDOM } from "react";
+import App from "./App";
+
+
+
+ReactDOM.render(document.getElementById('root'));
