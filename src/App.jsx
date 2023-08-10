c254424a66fe22a9c02a85a2d4a10acefe4e6394
Pedro Haka <pedrohaka94@gmail.com>
2023-08-10 05:30:46 -0300

    create base layout using bootstrap/sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import Header from './components/header/Header';
+import Sidebar from './components/sidebar/Sidebar';
+import HomeScreen from './screens/homeScreen/HomeScreen';
+import { Container } from 'react-bootstrap';
+import './_app.scss'
 
 const App = () => {
-    return <div>Blutube</div>
+    return (
+        <>
+            <Header />
+            <div className="app__container border border-info">
+                <Sidebar />
+                <Container fluid className='app__main border border-warning'>
+                    <HomeScreen />
+                </Container>
+            </div>
+
+        </>
+    )
 }
 
 export default App;
---
+++ src/components/categoriesbar/Categoriesbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './_categoriesbar.scss'
+
+function Categoriesbar() {
+    return (
+        <div>
+            Categories Bar
+        </div>
+    )
+}
+
+export default Categoriesbar
