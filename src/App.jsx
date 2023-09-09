55eb44b008208675d44066fe75050fc1e4576c39
Pedro Haka <pedrohaka94@gmail.com>
2023-09-09 17:24:08 -0300

    Create Layout Component

--- src/App.jsx
+++ src/App.jsx
@@ -6,31 +6,35 @@ import Sidebar from './components/sidebar/Sidebar';
 import HomeScreen from './screens/homeScreen/HomeScreen';
 import { Container } from 'react-bootstrap';
 import LoginScreen from './screens/loginScreen/LoginScreen';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
-const App = () => {
 
+const Layout = ({ children }) => {
     const [sidebar, toggleSidebar] = useState(false);
 
     const handleToggleSidebar = () => toggleSidebar(value => !value);
 
     return (
-        /* <>
+        <>
             <Header handleToggleSidebar={handleToggleSidebar} />
             <div className="app__container">
-                <Sidebar 
-                    sidebar={sidebar} 
-                    handleToggleSidebar={handleToggleSidebar} 
+                <Sidebar
+                    sidebar={sidebar}
+                    handleToggleSidebar={handleToggleSidebar}
                 />
                 <Container fluid className='app__main'>
-                    <HomeScreen />
+                    {children}
                 </Container>
             </div>
 
-        </> */
-        
+        </>
+    )
+}
+
+const App = () => {
+    return (
         <LoginScreen />
-        
     )
 }
 
--- src/screens/loginScreen/LoginScreen.jsx
+++ src/screens/loginScreen/LoginScreen.jsx
@@ -5,6 +5,7 @@ const LoginScreen = () => {
   return (
     <div className="login">
         <div className="login__container">
+            <h3>BluTube</h3>
             <img src="https://logodownload.org/wp-content/uploads/2014/10/youtube-logo-5-2.png" alt="BluTube Logo" />
             <button>Login with Google</button>
             <p>This project uses YouTube Data API</p>
