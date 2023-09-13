8dba536ebde9834f78260ee896656583e40ff9d1
Pedro Haka <pedrohaka94@gmail.com>
2023-09-12 21:54:49 -0300

    Criar e Configurar Redux Store

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './_base.scss';
+import { Provider } from 'react-redux';
+import store from './redux/store.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
---
+++ src/redux/store.js
@@ -0,0 +1,24 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+
+const preloadedState = {
+    name: 'Summit',
+    age: '21'
+}
+
+const reducer = (preloadedState) => preloadedState;
+
+//As of Redux v6, 'createStore()' has been deprecated in favor of 'configureStore()'.
+//Please keep in mind that 'configureStore' uses 'createStore' under the hood, as well
+//as 'applyMiddleware', 'composeWithDevTools', enchancers and more.
+//Also keep in mind that 'configureStore()' accepts ONLY ONE object as argument.
+//This object contains all the store configurations like the reducer function/array, 
+//preloadedState, used middlewares, etc.
+const store = configureStore({
+    reducer: reducer,
+    preloadedState
+});
+
+export default store;
+
+
